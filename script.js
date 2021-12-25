82b129bd3ad56383b29a11ce46f283121d2451c5
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-25 17:22:38 +0100

    tot6

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Print 1 - 135
+//1 Print 1 - 135
 for(var i=1;i<=35;i++){
     //Show in browser console
     console.log(i);
@@ -6,7 +6,7 @@ for(var i=1;i<=35;i++){
     document.getElementById("box1").innerHTML += `Number: ${i} <br/>`;
 }
 
-//Print Odd Numbers 1 - 135
+//2 Print Odd Numbers 1 - 135
 for(var i=1;i<=35;i = i+2){
     //Show in browser console
     console.log(i);
@@ -14,7 +14,7 @@ for(var i=1;i<=35;i = i+2){
     document.getElementById("box2").innerHTML += `Odd Number: ${i} <br/>`;
 }
 
-//Print 1 - 135
+//3 Print 1 - 135
 let sum = 0;
 for(var i=0; i<=35 ; i++ ){
     //Show in browser console
@@ -24,9 +24,44 @@ for(var i=0; i<=35 ; i++ ){
     document.getElementById("box3").innerHTML += `Number is: ${i} Sum: ${sum} <br/>`;
 }
 
-//Print the elements of an array
+//4 Print the elements of an array
 X = [1,4,2,12];
 
 for(var i=0; i < X.length ; i++){
     console.log(X[i])
+    //Show in HTML
+    document.getElementById("box4").innerHTML += `Result is: ${X[i]} <br/>`;
 }
+
+
+//5 Find Max
+
+X = [0,2,-3,-1,8,14];
+var max = X[0];
+var min = X[0];
+
+for(var i=0; i < X.length ; i++){
+    if(max < X[i]){
+        var max = X[i];
+    } else if(min > X[i]){
+        var min = X[i];
+    }
+}
+//Show in browser console
+console.log(max);console.log(min);
+//Show in HTML
+document.getElementById("box5").innerHTML += `Max is: ${max} Min is: ${min} <br/>`;
+
+
+//6 Get Average
+X = [2,1,3];
+var boxNm = 0;
+
+for(var i=0; i < X.length ; i++){
+    boxNm = boxNm + X[i];
+    console.log(boxNm)
+}
+
+var average = boxNm / X.length;
+console.log(average)
+document.getElementById("box6").innerHTML += `Average is: ${average} <br/>`;
